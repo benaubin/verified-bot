90d0d83cfa902ccaa2486ae543a43cb5e4342549
Nicholas Orlowsky <nickorlow@cavcash.com>
2022-01-05 16:40:59 -0600

    moved get user out to function and minor reformatting

--- utdir-ldap-processor/src/main.rs
+++ utdir-ldap-processor/src/main.rs
@@ -1,22 +1,25 @@
 use ldap3::result::Result;
 use ldap3::{LdapConn, Scope, SearchEntry};
 
-fn main() -> Result<()> {
-    let eid = "no3969";
+fn main() {
+    println!("{}", get_ldap_user_json("no3969").unwrap());
+    println!("\n\n");
+    println!("{}", get_ldap_user_json("bha366").unwrap());
+}
 
+fn get_ldap_user_json(eid: &str) -> Result<String> {
     let mut ldap = LdapConn::new("ldap://directory.utexas.edu:389")?;
     let (rs, _res) = ldap
         .search(
             "dc=directory,dc=utexas,dc=edu",
             Scope::Subtree,
-            &("uid=".to_owned()+eid),
+            &format!("uid={}", eid),
             vec!["*"],
         )?
         .success()?;
 
     // Convert to a json string that could be returned by a REST API
     let json : String = format!("{:?}",&SearchEntry::construct(rs.first().unwrap().to_owned()))[12..].to_string();
-    println!("{}",json);
 
-    Ok(ldap.unbind()?)
+   return Ok(json);
 }
