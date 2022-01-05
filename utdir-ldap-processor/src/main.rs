4c4a9874c09af38ec0c7d3e786a14dc562ac83a1
Arpan Dhatt <ary.dhatt@gmail.com>
2022-01-05 12:07:28 -0600

    Initial Commit

---
+++ utdir-ldap-processor/src/main.rs
@@ -0,0 +1,23 @@
+use ldap3::{LdapConn, Scope, SearchEntry, SearchOptions};
+use ldap3::result::Result;
+
+fn main() -> Result<()> {
+    let mut ldap = LdapConn::new("ldap://directory.utexas.edu:389")?;
+    let (rs, _res) = ldap.search(
+        "dc=directory,dc=utexas,dc=edu",
+        Scope::Subtree,
+        "sn=Aubin",
+        vec!["*"]
+    )?.success()?;
+    print!("Content-type: text/html\n\n");
+    println!("<html>");
+        println!("<body>");
+            println!("<p>");
+                for entry in rs {
+                    println!("{:?}", SearchEntry::construct(entry));
+                }
+            println!("</p>");
+        println!("</body>");
+    println!("</html>");
+    Ok(ldap.unbind()?)
+}
