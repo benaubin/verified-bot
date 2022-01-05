ffec1309acdca017d276a6925206e573afc6c872
Arpan Dhatt <ary.dhatt@gmail.com>
2022-01-05 12:09:43 -0600

    rustfmt

--- utdir-ldap-processor/src/main.rs
+++ utdir-ldap-processor/src/main.rs
@@ -1,23 +1,25 @@
-use ldap3::{LdapConn, Scope, SearchEntry, SearchOptions};
 use ldap3::result::Result;
+use ldap3::{LdapConn, Scope, SearchEntry};
 
 fn main() -> Result<()> {
     let mut ldap = LdapConn::new("ldap://directory.utexas.edu:389")?;
-    let (rs, _res) = ldap.search(
-        "dc=directory,dc=utexas,dc=edu",
-        Scope::Subtree,
-        "sn=Aubin",
-        vec!["*"]
-    )?.success()?;
+    let (rs, _res) = ldap
+        .search(
+            "dc=directory,dc=utexas,dc=edu",
+            Scope::Subtree,
+            "sn=Aubin",
+            vec!["*"],
+        )?
+        .success()?;
     print!("Content-type: text/html\n\n");
     println!("<html>");
-        println!("<body>");
-            println!("<p>");
-                for entry in rs {
-                    println!("{:?}", SearchEntry::construct(entry));
-                }
-            println!("</p>");
-        println!("</body>");
+    println!("<body>");
+    println!("<p>");
+    for entry in rs {
+        println!("{:?}", SearchEntry::construct(entry));
+    }
+    println!("</p>");
+    println!("</body>");
     println!("</html>");
     Ok(ldap.unbind()?)
 }
